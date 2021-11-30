5c0dcd38717676d0896c65c79832a4e82c5e45af
Andrew Werner <awerner32@gmail.com>
2021-11-30 18:01:40 -0500

    minor cleanup

--- blog/blog.md
+++ blog/blog.md
@@ -172,25 +172,30 @@ Another cool augmented search tree is the interval tree which lets you search
 for overlaps. 
 
 ```go
-type pair [2]int
+
+type pair[T constraints.Ordered] [2]T
+func (p pair[T]) first() T  { return p[0] }
+func (p pair[T]) second() T { return p[1] }
+func (p pair[T]) compare(o pair[T]) int {
+	if c := ordered.Compare(p.first(), o.first()); c != 0 {
+		return c
+	}
+	return ordered.Compare(p.second(), o.second())
+}
+
 m := interval.MakeSet(
     ordered.Compare[int],
-    func(a, b pair) int {
-        if c := ordered.Compare(a[0], b[0]); c != 0 {
-            return c
-        }
-        return ordered.Compare(a[0], b[0])
-    },
-    func(i pair) int { return i[0] },
-    func(i pair) int { return i[1] },
+    pair[int].compare,
+    pair[int].first,
+    pair[int].second,
 )
-for _, p := range []pair{
+for _, p := range []pair[int]{
     {1, 2}, {2, 3}, {1, 5}, {0, 6}, {2, 7},
 } {
     m.Upsert(p)
 }
 it := m.Iterator()
-for it.FirstOverlap(pair{4, 5}); it.Valid(); it.NextOverlap() {
+for it.FirstOverlap(pair[int]{4, 5}); it.Valid(); it.NextOverlap() {
     fmt.Println(it.Cur())
 }
 // Output:
--- interval/example_test.go
+++ interval/example_test.go
@@ -15,32 +15,39 @@
 package interval_test
 
 import (
+	"constraints"
 	"fmt"
 
 	"github.com/ajwerner/btree/internal/ordered"
 	"github.com/ajwerner/btree/interval"
 )
 
+type pair[T constraints.Ordered] [2]T
+
+func (p pair[T]) compare(o pair[T]) int {
+	if c := ordered.Compare(p[0], o[0]); c != 0 {
+		return c
+	}
+	return ordered.Compare(p[1], o[1])
+}
+
+func (p pair[T]) first() T  { return p[0] }
+func (p pair[T]) second() T { return p[1] }
+
 func ExampleBlog() {
-	type pair [2]int
 	m := interval.MakeSet(
 		ordered.Compare[int],
-		func(a, b pair) int {
-			if c := ordered.Compare(a[0], b[0]); c != 0 {
-				return c
-			}
-			return ordered.Compare(a[0], b[0])
-		},
-		func(i pair) int { return i[0] },
-		func(i pair) int { return i[1] },
+		pair[int].compare,
+		pair[int].first,
+		pair[int].second,
 	)
-	for _, p := range []pair{
+	for _, p := range []pair[int]{
 		{1, 2}, {2, 3}, {1, 5}, {0, 6}, {2, 7},
 	} {
 		m.Upsert(p)
 	}
 	it := m.Iterator()
-	for it.FirstOverlap(pair{4, 5}); it.Valid(); it.NextOverlap() {
+	for it.FirstOverlap(pair[int]{4, 5}); it.Valid(); it.NextOverlap() {
 		fmt.Println(it.Cur())
 	}
 	// Output:
